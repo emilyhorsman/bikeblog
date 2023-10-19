54fcb2de55bfd8bbee30a06299f4847a922985c7
Emily Horsman <me@emilyhorsman.com>
2023-10-19 15:01:57 -0700

    exclude banner images from full scale webp gen

--- images.js
+++ images.js
@@ -10,7 +10,9 @@ async function resize(file) {
   const small = original.clone().resize(baseWidth);
 
   const name = path.basename(file.name, ".jpg");
-  await original.toFile(name + ".webp");
+  if (!file.name.includes("16x9")) {
+    await original.toFile(name + ".webp");
+  }
   await large.toFile(name + "_2x.webp");
   await large.toFile(name + "_2x.avif");
   await medium.toFile(name + "_15x.webp");
