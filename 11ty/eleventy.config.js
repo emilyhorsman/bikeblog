7f25368d721fbc593dba522179b91a37d76f3720
Emily Horsman <me@emilyhorsman.com>
2023-12-30 22:33:48 -0800

    with liquid

--- 11ty/eleventy.config.js
+++ 11ty/eleventy.config.js
@@ -1,11 +1,6 @@
 import Sass from "sass";
-import EleventyWebC from "@11ty/eleventy-plugin-webc";
 
 export default (c) => {
-  c.addPlugin(EleventyWebC, {
-    components: "src/_components/**/*.webc",
-  });
-
   c.addFilter("css", function (value) {
     return Sass.compileString(value, { style: "compressed" }).css;
   });
