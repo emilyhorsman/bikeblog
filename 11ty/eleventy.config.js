a4f4d20936ba10988d4a947c08c78eb541729354
Emily Horsman <me@emilyhorsman.com>
2023-12-31 04:16:07 -0800

    images

--- 11ty/eleventy.config.js
+++ 11ty/eleventy.config.js
@@ -1,5 +1,7 @@
 import * as Sass from "sass";
 import * as htmlmin from "html-minifier";
+import Image, * as EleventyImg from "@11ty/eleventy-img";
+import * as path from "path";
 
 export default (c) => {
   c.addFilter("css", function (value) {
@@ -19,6 +21,33 @@ export default (c) => {
       collapseWhitespace: true,
       collapseInlineTagWhitespace: true,
     });
+  });
+
+  c.addShortcode("image", async function (naiveSrc, alt, width = 768) {
+    const src = path.join(
+      naiveSrc[0] === "/"
+        ? this.eleventy.directories.input
+        : path.dirname(this.page.inputPath),
+      naiveSrc,
+    );
+    const outputDir = path.join(this.eleventy.directories.output, "img");
+    const metadata = await Image(src, {
+      widths: [1, 1.5, 2].map((scale) => width * scale),
+      formats: ["avif", "webp"],
+      outputDir,
+    });
+    const reference = await Image(src, {
+      widths: ["auto"],
+      formats: ["webp"],
+      outputDir,
+    });
+
+    const pictureElement = EleventyImg.generateHTML(metadata, {
+      alt,
+      sizes: "auto",
+    });
+
+    return `<div class="image-container"><a href="${reference.webp[0].url}" class="image" target="_blank" style="max-width:${metadata.webp[0].width}px">${pictureElement}</a></div>`;
   });
 
   return {
