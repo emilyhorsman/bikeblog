b0fc4bb94c423ce645950908be39f1744630640c
Emily Horsman <me@emilyhorsman.com>
2023-12-31 01:50:19 -0800

    fix posts tag showing up

--- 11ty/src/posts/posts.11tydata.js
+++ 11ty/src/posts/posts.11tydata.js
@@ -1,4 +1,9 @@
 export default {
   layout: "post.liquid",
   tags: ["posts"],
+  eleventyComputed: {
+    postTags: (data) => {
+      return data.tags.filter((tag) => tag !== "posts");
+    },
+  },
 };
